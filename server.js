2bca8cf13dfd9ab3826c6c5a312188e01c56478d
Hari <stonefrozen096@gmail.com>
2025-10-26 17:31:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
